ae9ca9c5408b09e1ea386d5ec2980ba6b3ef767a
Aybars Acar <aybars.dev@gmail.com>
2022-10-23 21:22:33 +1100

    small changes

---
+++ src/main/java/com/dreamteam/persistance/repositories/JobRepositoryImpl.java
@@ -0,0 +1,21 @@
+package com.dreamteam.persistance.repositories;
+
+import com.dreamteam.domain.entities.Job;
+import com.dreamteam.domain.repositories.JobRepository;
+
+public class JobRepositoryImpl implements JobRepository {
+  @Override
+  public void create(Job job) {
+
+  }
+
+  @Override
+  public void modify(Job job) {
+
+  }
+
+  @Override
+  public void deleteById(long id) {
+
+  }
+}
